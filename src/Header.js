e50da026cf6698a3ada6c4cbf4d7da15216169ee
pradz13 <pradipta.nag2@gmail.com>
2021-09-18 10:36:13 +0530

    Small Changes

--- src/Header.js
+++ src/Header.js
@@ -1,11 +1,13 @@
 
 
 const Header = () => {
-    return (<header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
-        <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
-            <span className="fs-4">Review that matters</span>
-        </a>
-    </header>);
+    return (
+        <header className="d-flex flex-wrap justify-content-center py-3 mb-4 border-bottom">
+            <a href="/" className="d-flex align-items-center mb-3 mb-md-0 me-md-auto text-dark text-decoration-none">
+                <i><span className="fs-2">Review that matters</span></i>
+            </a>
+        </header>
+    );
 }
 
 export default Header;
